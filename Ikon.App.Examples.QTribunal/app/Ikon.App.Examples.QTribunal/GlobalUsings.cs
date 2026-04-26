6b029bad47c88e8969c0447dc366743e6a65c3de
Ikon Machine User <admin@ikon.live>
2026-04-26 08:43:49 +0200

    Update Ikon.App.Examples.QTribunal

--- Ikon.App.Examples.QTribunal/app/Ikon.App.Examples.QTribunal/GlobalUsings.cs
+++ Ikon.App.Examples.QTribunal/app/Ikon.App.Examples.QTribunal/GlobalUsings.cs
@@ -7,6 +7,6 @@ global using Ikon.Common.Core.Reactive;
 global using Ikon.Common.Core;
 global using Ikon.Common;
 global using Ikon.Parallax.Components.Standard;
-global using Ikon.Parallax.Themes.Default;
+global using Ikon.Parallax.Themes.Ikon;
 global using Ikon.Parallax;
 global using Ikon.Server;
