5f1d3d684dd8576935018bd4615c9af7885ed1f9
Ikon Machine User <admin@ikon.live>
2026-06-02 09:41:37 +0200

    Update Ikon.App.Examples.Live2DChat

--- Ikon.App.Examples.Live2DChat/app/Ikon.App.Examples.Live2DChat/GlobalUsings.cs
+++ Ikon.App.Examples.Live2DChat/app/Ikon.App.Examples.Live2DChat/GlobalUsings.cs
@@ -11,7 +11,7 @@ global using Ikon.Common.Core.Reactive;
 global using Ikon.Common.Core.Scope;
 global using Ikon.Common.Core;
 global using Ikon.Common;
-global using Ikon.Parallax.Themes.Ikon;
+global using Ikon.Parallax.Theming;
 global using Ikon.Parallax;
 global using Ikon.Pipeline.Items;
 global using Ikon.Pipeline;
